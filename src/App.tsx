96a50480be4c1eaf29bb02cdbc5733542efe5935
Manish Bansal <manibans@in.ibm.com>
2024-10-23 18:11:47 +0530

    empty template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+      <h1>Empty template ready for use</h1>
+      <h1>React + TypeScript + Vite + Tailwind + Chadcn</h1>
+    </>
+  )
+}
+
+export default App
